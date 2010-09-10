775efdf1a79bd628b1809e70bae75690aef5ce18
Takeru <tsutomu774@gmail.com>
2010-09-10 23:28:28 +0200

    initial commit djwachou

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Djwachou::Application.config.secret_token = 'ff562a79e2a062203cfe52daca2abea72f7b83448e4057b3437570cbbddee9a0bbcaa5210691b61dbbaba886fd43822a3b9e476d9ccb05fc88ff8c59f6e9ec33'
